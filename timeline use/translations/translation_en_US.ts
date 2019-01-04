cf799a5cf95654afcb437610bf7dc8c237cb1009
Paul Engelhardt <1paulengelhardt@gmail.com>
2019-01-04 09:41:07 +0100

    hand bewegung

---
+++ timeline use/translations/translation_en_US.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Take a present</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Take a present</translation>
+        </message>
+        <message>
+            <source>Please take a Present</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Please take a Present</translation>
+        </message>
+        <message>
+            <source>Please take your present</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Please take your present</translation>
+        </message>
+        <message>
+            <source>Have a great evening</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Have a great evening</translation>
+        </message>
+        <message>
+            <source>Have a good evening</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Have a good evening</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Please take your present</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Please take your present</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>follow T series</source>
+            <comment>Text</comment>
+            <translation type="obsolete">follow T series</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline/behavior_layer1/keyframe1/Say</name>
+        <message>
+            <source>Take a present</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Take a present</translation>
+        </message>
+    </context>
+</TS>
