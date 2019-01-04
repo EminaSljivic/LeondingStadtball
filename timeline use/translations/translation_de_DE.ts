00eaf2d3c23f2c466bb9829e1b4d52d4e9b16ccb
Simon Holzapfel <s.holzapfel@HTBLALeonding.onmicrosoft.com>
2019-01-04 10:04:56 +0100

    update

---
+++ timeline use/translations/translation_de_DE.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hallo</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hier, ein Geschenk für Sie!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hier, ein Geschenk für Sie!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Haben Sie einen Schönen Abend!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Haben Sie einen Schönen Abend!</translation>
+        </message>
+    </context>
+</TS>
